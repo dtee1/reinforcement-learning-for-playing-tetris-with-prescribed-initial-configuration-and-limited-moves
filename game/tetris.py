96e2d100be3cfa27092f07e8c95ae38d033ede99
Tag <tgaud061@uottawa.ca>
2023-12-10 18:32:00 -0500

    Added a carving algorithm based initial config

--- game/tetris.py
+++ game/tetris.py
@@ -1,5 +1,6 @@
 import numpy as np
 from random import randint, getrandbits
+import time
 
 tetrominos = (
     (
@@ -37,40 +38,132 @@ tetrominos = (
     )
 )
 
+def get_tetromino(piece: int, rotations: int) -> tuple[np.array, tuple[int, ...]]:
+    return tetrominos[piece][rotations % len(tetrominos[piece])]
+
 class Tetris:
     def __init__(self, L: int, M: int, random_pieces=False):
-        self.board, self.pieces = self.__get_initial_config()
         self.L = L
         self.M = M
+        self.random_pieces = random_pieces
+
+        while True:
+            self.board = None
+            self.pieces = []
+
+            if self.__get_initial_config():
+                break
         self.lines_cleared = 0
         self.moves_used = 0
         self.state = None
-        self.random_pieces = random_pieces
+        
+    # Carving Approach
+    def __get_initial_config(self) -> None:
+        # Initial full board generation
+        initial_empty = 20 - self.L # randint(0, 20 - self.L)
 
-    def __get_initial_config(self) -> tuple[np.array, tuple[int, ...]]:
-        return np.full((20, 10), False, dtype=bool), [randint(0, 6) for _ in range(2)]
-    
-    def get_state(self) -> tuple[np.array, int, int, int, int, bool]:
-        return self.board, self.pieces[0], self.pieces[1], self.L - self.lines_cleared, self.M - self.moves_used, self.state
+        self.board = np.full((20, 10), True, dtype=bool)
+        self.board[:initial_empty, :] = False
+
+        pieces = list(range(7))
+        checkpoints = [np.copy(self.board)]
+
+        checkpoint_loop = 0
+        revert_loop = 0
+
+        while (len(self.pieces) < self.M) and np.all(self.board[-1]):
+            # Choose a random piece from the pieces left
+            piece_index = randint(0, len(pieces) - 1)
+            piece = pieces[piece_index]
+            rotations = randint(0, 3)
+            tetromino_width = get_tetromino(piece, rotations)[0].shape[1]
+            location = randint(0, 10-tetromino_width)
+
+            if self.carve(piece, rotations, location):
+                checkpoint_loop = 0
+                self.pieces.insert(0, piece)
+                del pieces[piece_index]
+                if not pieces:
+                    revert_loop = 0
+                    checkpoints.append(np.copy(self.board))
+                    pieces = list(range(7))
+            else:
+                if revert_loop > 100:
+                    if len(checkpoints) > 1:
+                        # Delete the last checkpoint
+                        checkpoints.pop()
+                        # Load from the new last checkpoint
+                        self.board = np.copy(checkpoints[-1])
+                        self.pieces = self.pieces[-((len(checkpoints) - 1) * 7):]
+                        pieces = list(range(7))
+                    
+                    checkpoint_loop = 0
+                    revert_loop = 0
+                elif checkpoint_loop > 100:
+                    # Load from the last checkpoint
+                    self.board = np.copy(checkpoints[-1])
+                    self.pieces = self.pieces[-((len(checkpoints) - 1) * 7):]
+                    pieces = list(range(7))
+
+                    checkpoint_loop = 0
+                    revert_loop += 1
+                else:
+                    checkpoint_loop += 1
+
+        return True
+
+    def carve(self, piece: int, rotations: int, location: int) -> bool:
+        tetromino, reverse_tetromino_topography = get_tetromino(piece, rotations)
+        tetromino_height, tetromino_width = tetromino.shape
+
+        # Calculate drop location
+        drop_deltas = self.calculate_drop_deltas(location, reverse_tetromino_topography, tetromino_width)
+        drop = self.calculate_drop(drop_deltas)
+        push = reverse_tetromino_topography[np.argmin(drop_deltas)] + 1
+
+        drop += push
+
+        # If the drop goes out of bounds then the carve failed
+        if drop + tetromino_height > 20:
+            return False
+
+        # Calculate overlap
+        board_slice = self.board[drop:drop + tetromino_height, location:location + tetromino_width]
+        overlap = np.all(np.logical_not(tetromino) | board_slice)
+
+        # If no overlap then carving failed
+        if not overlap:
+            return False
+        
+        # Apply the carve
+        self.board[drop:drop + tetromino_height, location:location + tetromino_width] ^= tetromino
+        
+        # Make a new drop to ensure it falls where the carve was
+        # Ensures that there were no blocking blocks above and that there were supporting blocks below
+        new_drop_deltas = self.calculate_drop_deltas(location, reverse_tetromino_topography, tetromino_width)
+        new_drop = self.calculate_drop(new_drop_deltas)
+        
+        # If the new drop does not land where the carve was done then revert the carve and carving failed
+        if new_drop != drop:
+            self.board[drop:drop + tetromino_height, location:location + tetromino_width] |= tetromino
+            return False
+
+        # Carving succeeded
+        return True
 
     def move(self, rotations: int, location: int) -> None:
         piece = self.pieces.pop(0)
         if self.random_pieces:
             self.pieces.append(bool(getrandbits(1)))
 
-        tetromino, reverse_tetromino_topography = tetrominos[piece][rotations % len(tetrominos[piece])]
+        tetromino, reverse_tetromino_topography = get_tetromino(piece, rotations)
 
         # Ensure the location is not out of bounds horizontally
         tetromino_width = tetromino.shape[1]
         location = min(location, 10-tetromino_width)
         
-        board_topography = []
-        for col in self.board.T[location:location+tetromino_width, :]:
-            result = np.where(col)[0]
-            board_topography.append(result[0] if len(result) != 0 else 20)
-
-        drop_deltas = np.array(board_topography) - np.array(reverse_tetromino_topography)
-        drop = min(drop_deltas) - 1
+        drop_deltas = self.calculate_drop_deltas(location, reverse_tetromino_topography, tetromino_width)
+        drop = self.calculate_drop(drop_deltas)
 
         # Check if out of bounds
         if drop < 0:
@@ -112,6 +205,26 @@ class Tetris:
 
         if self.moves_used >= self.M:
                 self.state = False
+    
+    def calculate_drop(self, drop_deltas) -> int:
+        return min(drop_deltas) - 1
+    
+    def calculate_drop_deltas(self, location, reverse_tetromino_topography, tetromino_width):
+        board_topography = []
+        for col in self.board.T[location:location+tetromino_width, :]:
+            result = np.where(col)[0]
+            board_topography.append(result[0] if len(result) != 0 else 20)
+
+        return np.array(board_topography) - np.array(reverse_tetromino_topography)
+
+    def get_state(self) -> tuple[np.array, int, int, int, int, bool]:
+        return self.board, self.pieces[0], self.pieces[1], self.L - self.lines_cleared, self.M - self.moves_used, self.state
             
     def reset(self) -> None:
         self.__init__(self.L, self.M, random_pieces=self.random_pieces)
+
+start_time = time.time()
+iterations = 100
+for i in range(iterations):
+    game = Tetris(15, 40)
+print(iterations / (time.time() - start_time))
